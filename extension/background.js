d459d0a662839fb3d7bfa40d0a2f2161abe50aff
swoollaston3 <77474074+swoollaston3@users.noreply.github.com>
2021-07-31 18:47:29 +1200

    cleaned up extension code

--- extension/background.js
+++ extension/background.js
@@ -2,25 +2,19 @@
 
 const server = "https://questionv2-yk4rgxamaa-ts.a.run.app/";
 
-//var questions = [];
-
-//function showQuestions() {
-////  var temperatures = [59.2, 60.1, 63, 65, 62].map(function (t, i) {
-////    return 'The temperature at ' + (i || 'noon') + ' was ' + t
-////  })
+//chrome.action.onClicked.addListener(tab => { alert('working?'); });
 //
-//    // from https://stackoverflow.com/questions/43567079/simpler-way-to-print-array-items-into-list-items-javascript
-//    document.getElementById('questions').innerHTML =
-//    '<li>' + questions.join('</li><li>') + '</li>';
+//function changeIcon(){
+//    chrome.action.setIcon({ path: "imgs/icon16_gray.png" });
 //}
-
-//chrome.browserAction.onClicked.addListener(function(tab) { alert('icon clicked')});
-
-
-//chrome.browserAction.onClicked.addListener(function(tab) {
-//    alert('working?');
+//
+//
+//chrome.action.onClicked.addListener((tab) => {
+//    chrome.scripting.executeScript({
+//        target: { tabId: tab.id },
+//        function: changeIcon
+//  });
 //});
-
 
 // listen for messages
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
@@ -46,62 +40,15 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
                 res.json().then(function(data){
 
                     response(data);
-
-//                    var jsonData = JSON.parse(data);
-
-                        //response({question: data.questions.question, target: data.target, sentence_number: data.sentence_number});
-
-
-
-
                     console.log(data);
 
-                    //showQuestions();
                 });
             }).catch(function(err) {
                 response({question: 'Error - no question', target: "Error - no target", sentence_number: "Error - no sentence number"});
             });
-
         });
 
     }
-
     return true;
 
 });
-
-
-
-//
-//    const questions = [
-//      "A kind of Japanese woodblock print, privately commissioned for special occasions such as the New Year.",
-//      "In <em>fencing</em>, the ninth and last thrust, usually aimed at the side.",
-//      "The point in a solar orbit where the orbiting body is closest to the sun.",
-//      "A typewriter used to emboss paper with braille cells to be read by the visually impaired instead of using a manual stylus.",
-//      "A fire produced by the friction of one piece of wood upon another, or of a rope upon a stake of wood."
-//    ];
-//
-//    const targets = [
-//      "surimono",
-//      "flanconade",
-//      "perihelion",
-//      "brailler",
-//      "needfire"
-//    ];
-//
-//    const sentenceNumbers = [
-//        5,
-//        3,
-//        5,
-//        23,
-//        1
-//    ]
-//
-//    var number = 3
-//
-//    // send response
-//    response({question: questions[number], target: targets[number], senNum: sentenceNumbers[number]});
-//
-//  }
-//
-//});
--- extension/content.js
+++ extension/content.js
@@ -1,29 +1,12 @@
 // from https://github.com/btahir/summarlight/blob/master/extension_bundle/content.js
-//alert("Generating questions...");
 
-
-// send message To Background
+// send message To background.js
 chrome.runtime.sendMessage({name: "fetchQuestions"}, (response) => {
 
   // wait for Response
-
     console.log("Response in content.js");
     console.log(response);
 
-//  document.querySelector('h1').innerHTML = response.question;
-//  document.querySelector('h2').innerHTML = response.target;
-//  document.querySelector('strong').innerHTML = response.sentence_number;
-
-    // for each value
-
-//    var el = document.body;
-//    text = document.querySelectorAll("p");
-//    console.log(text);
-
-//    for (let k = 0; k < text.length; k++) {
-//
-//            console.log(k + text[k]);
-//    }
     var previously_targetted = []
 
     for (var i = 0; i < response.questions.length; i++) {
@@ -34,76 +17,35 @@ chrome.runtime.sendMessage({name: "fetchQuestions"}, (response) => {
         var sentence_number = question_object.sentence_number;
         var target = question_object.target;//.replace(/^["'](.+(?=["']$))["']$/, '$1');
 
+        // try not to double up in the same area
         if (previously_targetted.includes(target)){
             continue;
         }
 
         previously_targetted.push(target);
 
-
-
         console.log(question);
 
         var split_on_colon = question.split(":");
 
+        // get the bloom level and question out
         var bloom_level = split_on_colon[0];
         question = split_on_colon[1];
 
-        //value = target;
-
-        //highlight(value);
-
-        //value = unicodeToChar(value).replace(/\\n/g, '');
-        //document.body.innerHTML = document.body.innerHTML.split(value).join('<span style="background-color: #fff799;">' + value + '</span>');
-
         var el = document.body; // getElementById('mytext');
-
-        // text = el.innerHTML;
-
-        text = document.querySelectorAll("section, p, article");
-
+        text = document.querySelectorAll("section, p, article"); // tags with the main text
         tooltip_text = "<div><strong>"+ bloom_level +"</strong></div>" + question;
 
-        /*
-        <div class="tooltip">
-            Right
-            <div class="right">
-                <img src="cssttp/tooltip-head.jpg" />
-                <div class="text-content">
-                    <h3>Fade in Effect</h3>
-                    <ul>
-                        <li>This demo has fade in/out effect.</li>
-                        <li>It is using CSS opacity, visibility, and transition property to toggle the tooltip.</li>
-                        <li>Other demos are using display property<em>(none or block)</em> for the toggle.</li>
-                    </ul>
-                </div>
-                <i></i>
-            </div>
-        </div>
-        */
-
-        // tooltip_text = question;
-
-
-
-
-
         for (let j = 0; j < text.length; j++) {
+
             let tag = text[j].tagName;
-
             p_text = text[j].innerText;
-            // console.log(p_text);
-
             p_text = p_text.replace(/[[0-9]+]+/g, ""); // remove wiki references
-            //p_text = p_text.lower();
-
             // console.log(p_text); // after reference removal
 
             if (p_text.toLowerCase().includes(target.toLowerCase())){
 
-
-
-                //var pattern = new RegExp(target, 'gi')
+                // add the question as a tooltip
                 text[j].innerHTML = text[j].innerHTML.replace(target, "<span class='tooltip'><mark>"
                 + target + "</mark><span class='right'><span class='text-content'>"
                 + tooltip_text + "</span><i></i></span></span>");
@@ -111,35 +53,8 @@ chrome.runtime.sendMessage({name: "fetchQuestions"}, (response) => {
 
             }
 
-
-            // working ok for small phrases
-//            if (text[j].innerHTML.match(target) && !(tag == "BUTTON" || tag == "SPAN")){
-//                text[j].innerHTML = text[j].innerHTML.replace(new RegExp(target, 'g'),"<mark><div class=\"tooltip\">"+target+"<span class=\"tooltiptext\">" + tooltip_text + "</span></div></mark>");
-//            }
-
         }
-
-        /*
-
-        <div class="tooltip">Hover over me
-            <span class="tooltiptext">Tooltip text</span>
-         </div>
-
-        */
-
-
-        //console.log(el.innerText);
-
-        //keyword = value; //document.getElementById('input').value;
-
-        //marked = text.replace(new RegExp(keyword, 'g'),"<mark>"+keyword+"</mark>");
-
-        //el.innerHTML = marked;
-
-
     }
-
-
 });
 
 
